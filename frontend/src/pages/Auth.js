efbc540c7d4f217263873f7417cb5ef43179b862
Saadman Sakib <skilzer@my.yorku.ca>
2021-12-17 21:50:08 +0600

    User Signup-Added

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -5,7 +5,8 @@ import './Auth.css';
 class AuthPage extends Component {
   state = {
     isLogin: true
-  };
+  }
+
 
   constructor(props) {
     super(props);
@@ -14,54 +15,56 @@ class AuthPage extends Component {
   }
 
   switchModeHandler = () => {
-    this.setState(prevState => {
-      return { isLogin: !prevState.isLogin };
-    });
-  };
+   this.setState(prevState => {
+     return { isLogin: !prevState.isLogin };
+   });
+ };
 
   submitHandler = event => {
-    event.preventDefault();
-    const email = this.emailEl.current.value;
-    const password = this.passwordEl.current.value;
 
-    if (email.trim().length === 0 || password.trim().length === 0) {
-      return;
-    }
+      event.preventDefault();
+      const email = this.emailEl.current.value;
+      const password = this.passwordEl.current.value;
 
-    let requestBody = {
-      query: `
-        query {
-          login(email: "${email}", password: "${password}") {
+      if (email.trim().length === 0 || password.trim().length === 0) {
+        return;
+      }
+
+      let requestBody = {
+        query: `
+          query {
+            login(email: "${email}", password: "${password}") {
             userId
             token
             tokenExpiration
           }
         }
-      `
-    };
-
-    if (!this.state.isLogin) {
-      requestBody = {
-        query: `
-          mutation {
-            createUser(userInput: {email: "${email}", password: "${password}"}) {
-              _id
-              email
-            }
-          }
         `
       };
-    }
 
-    fetch('http://localhost:8000/graphql', {
+      if (!this.state.isLogin){
+        requestBody = {
+          query: `
+            mutation {
+              createUser(userInput: {email: "${email}", password: "${password}"}) {
+                _id
+                email
+              }
+            }
+            `
+          };
+        }
+
+
+      fetch('http://localhost:8000/graphql', {
       method: 'POST',
       body: JSON.stringify(requestBody),
       headers: {
         'Content-Type': 'application/json'
       }
-    })
+      })
       .then(res => {
-        if (res.status !== 200 && res.status !== 201) {
+        if(res.status !== 200 && res.status !==201) {
           throw new Error('Failed!');
         }
         return res.json();
@@ -71,27 +74,27 @@ class AuthPage extends Component {
       })
       .catch(err => {
         console.log(err);
-      });
+    });
   };
 
   render() {
     return (
       <form className="auth-form" onSubmit={this.submitHandler}>
         <div className="form-control">
-          <label htmlFor="email">E-Mail</label>
-          <input type="email" id="email" ref={this.emailEl} />
+          <label htmlFor ="email">E-Mail</label>
+          <input type="email" id="email" ref={this.emailEl}/>
         </div>
         <div className="form-control">
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" ref={this.passwordEl} />
+        <label htmlFor ="password">Password</label>
+        <input type="password" id="password" ref={this.passwordEl} />
         </div>
-        <div className="form-actions">
+        <div className="form-action">
           <button type="submit">Submit</button>
           <button type="button" onClick={this.switchModeHandler}>
-            Switch to {this.state.isLogin ? 'Signup' : 'Login'}
+          Switch to {this.state.isLogin ? 'Signup' : 'Login'}
           </button>
         </div>
-      </form>
+        </form>
     );
   }
 }
